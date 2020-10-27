3c428dcde65c0d94833eec4c8198fd25a21bbfc0
kra-ti <72553178+kra-ti@users.noreply.github.com>
2020-10-27 18:51:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ car.velocityX=speed;
 function draw() {
   background(255,255,255);  
 
-if(wall.x-car < (car.width+wall.width)/2){
+if(wall.x-car.x < (car.width+wall.width)/2){
 car.velocityX=0;
 var deformation=0.5*weight*speed*speed/22509;
 if (deformation>180 && deformation>180){
